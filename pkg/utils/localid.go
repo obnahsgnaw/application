0461bbce9ed55b35fa7ddf98e1b4b2f699133b46
wangsb <wangsb@efly-cetc.com>
2023-08-19 17:14:38 +0800

    perf(utils): perf

--- pkg/utils/localid.go
+++ pkg/utils/localid.go
@@ -3,13 +3,12 @@ package utils
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"github.com/obnahsgnaw/application/pkg/security"
 	"strconv"
 	"time"
 )
 
 func GenLocalId(prefix string) string {
-	return prefix + "_" + genMd5(strconv.FormatInt(time.Now().UnixNano(), 10)+security.RandAlphaNum(10))
+	return prefix + "_" + genMd5(strconv.FormatInt(time.Now().UnixNano(), 10)+RandAlphaNum(10))
 }
 
 func genMd5(str string) string {
