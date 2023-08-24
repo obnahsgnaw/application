a63d0d0066fe8762257177867f790ef027b061f7
obnahsgnaw <obnahsgnaw@qq.com>
2023-08-24 09:48:25 +0800

    perf(servertype): perf hdl type

--- servertype/servertype.go
+++ servertype/servertype.go
@@ -3,12 +3,14 @@ package servertype
 type ServerType string
 
 const (
-	Api ServerType = "api"
-	Rpc ServerType = "rpc"
-	Tcp ServerType = "tcp"
-	Wss ServerType = "wss"
-	Udp ServerType = "udp"
-	Hdl ServerType = "hdl"
+	Api    ServerType = "api"
+	Rpc    ServerType = "rpc"
+	Tcp    ServerType = "tcp"
+	Wss    ServerType = "wss"
+	Udp    ServerType = "udp"
+	TcpHdl ServerType = "tcp-hdl"
+	WssHdl ServerType = "wss-hdl"
+	UdpHdl ServerType = "upd-hdl"
 )
 
 func (s ServerType) String() string {
